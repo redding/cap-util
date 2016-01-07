5aae272736bec1dc9d2547e1c08c48ef1ae92daf
Kelly Redding <kelly@kellyredding.com>
2016-01-07 09:57:42 -0600

    rework `RunCmd` as `BundlerCmd`
    When trying to work in the run cmd in a production app the modeling
    didn't feel right - it felt forced.  The run cmd proved to be too
    generic to have much meaning but not generic enough to have widespread
    utility (something that generic isn't really necessary anyway).
    
    In our production apps, I found myself wanting to only use the run
    cmd on cap tasks that executed system cmds that required `bundle exec`.
    This prompted me to rework the run cmd as a bundler cmd.  This new
    version is identical to the run cmd except it adds `bundle exec` for
    you and should only be used when the cmd root contains a Gemfile and
    the cmd needs a bundled load path etc.
    
    This also adds a test helper for asserting a util is a correct bundler
    cmd util.

---
+++ lib/cap-util/bundler_cmd.rb
@@ -0,0 +1,39 @@
+require 'cap-util'
+
+module CapUtil
+
+  # This defines a base utiltiy for building bundler commands to run.
+
+  class BundlerCmd
+    include CapUtil
+
+    attr_reader :cmd
+
+    def initialize(cap, cmd, opts = nil)
+      opts ||= {}
+      @cap = cap
+
+      opts[:root] ||= :current_path
+      opts[:env]  ||= ""
+
+      cmd_root = @cap.send(opts[:root])
+
+      @cmd = "cd #{cmd_root} && #{opts[:env]} bundle exec #{cmd}"
+    end
+
+    def run; super(@cmd); end
+
+    module TestHelpers
+
+      def assert_bundler_cmd(util, *args)
+        assert_kind_of BundlerCmd, util
+
+        exp = BundlerCmd.new(*args)
+        assert_equal exp.cmd, util.cmd
+      end
+
+    end
+
+  end
+
+end
